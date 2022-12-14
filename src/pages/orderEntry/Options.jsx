944bd72008667872064743066c026e30d352dfb0
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-12-14 12:41:20 -0800

    add alert banner

--- src/pages/orderEntry/Options.jsx
+++ src/pages/orderEntry/Options.jsx
@@ -3,9 +3,11 @@ import axios from "axios";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     //optionType is either 'scoops' or 'toppings'
@@ -13,19 +15,23 @@ const Options = ({ optionType }) => {
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        console.log(error);
+        setError(true);
       });
   }, [optionType])
  
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
   const renderItems = items.map((item) => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />)
+  
+  if (error) {  
+    return <AlertBanner />;  
+  }
 
   return (
     <Row>
       {renderItems}
     </Row>
-  );
+  ); 
 };
 
 export default Options;
